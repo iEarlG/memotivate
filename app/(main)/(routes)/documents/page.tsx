7faad8c8d0504d39da4ca6ec5abbde733e0573c6
Earl A. Villapaz <jasonburning16@gmail.com>
2023-11-14 20:50:33 +0800

    8: API first call

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,13 +2,27 @@
 
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 
 import { PlusCircle } from "lucide-react";
 
+import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
     const { user } = useUser();
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating new notes",
+            success: "Notes created",
+            error: "Failed to create notes"
+        })
+    };
 
     return (
         <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -29,7 +43,7 @@ const DocumentsPage = () => {
             <h2 className="text-lg font-medium">
                 Welcome to Memotivate <span className="dark:text-emerald-700">{user?.firstName}</span>
             </h2>
-            <Button>
+            <Button onClick={onCreate}>
                 <PlusCircle className="h-4 w-4 mr-2" />
                 Start Memotivate
             </Button>

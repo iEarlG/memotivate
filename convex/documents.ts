cf94a39c415fa4accd8db7104684c8c756d644ba
Earl A. Villapaz <jasonburning16@gmail.com>
2023-11-24 20:14:12 +0800

    13: Archive Convex Function

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,7 +1,7 @@
 import { v } from "convex/values";
 
 import { mutation, query } from "./_generated/server";
-import { Doc } from "./_generated/dataModel";
+import { Doc, Id } from "./_generated/dataModel";
 
 export const getSidebar = query({
     args: {
@@ -53,6 +53,55 @@ export const create = mutation({
             isArchived: false,
             isPublished: false,
         });
+
+        return documents;
+    }
+});
+
+export const archive = mutation({
+    args: {id: v.id("documents")},
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocuments = await ctx.db.get(args.id);
+
+        if(!existingDocuments) {
+            throw new Error("Not found");
+        }
+
+        if (existingDocuments.userId !== userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const recurviseArchive = async(documentId: Id<"documents">) => {
+            const childrenDocument = await ctx.db
+            .query("documents")
+            .withIndex("by_user_parent", (q) => (
+                q.eq("userId", userId)
+                 .eq("parentDocument", documentId)
+            ))
+            .collect();
+
+            for (const child of childrenDocument) {
+                await ctx.db.patch(child._id, {
+                    isArchived: true,
+                });
+
+                await recurviseArchive(child._id);
+            }
+        };
+
+        const documents = await ctx.db.patch(args.id, {
+            isArchived: true,
+        });
+
+        recurviseArchive(args.id);
 
         return documents;
     }

1b19c1b351bfbebc2bd90ab7b8a5c913da7416d0
Earl A. Villapaz <jasonburning16@gmail.com>
2023-11-30 16:04:28 +0800

    16: restore, archive, removed Documents

--- convex/documents.ts
+++ convex/documents.ts
@@ -106,3 +106,109 @@ export const archive = mutation({
         return documents;
     }
 });
+
+export const getTrash = query({
+    handler: async(ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const userId = identity.subject;
+
+        const documents = await ctx.db
+            .query("documents")
+            .withIndex("by_user", (q) => q.eq("userId", userId))
+            .filter((q) => q.eq(q.field("isArchived"), true))
+            .order("desc")
+            .collect();
+        
+        return documents;
+    }
+});
+
+export const restoreDocs = mutation({
+    args: {id: v.id("documents")},
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocuments = await ctx.db.get(args.id);
+
+        if (!existingDocuments) {
+            throw new Error("Not found");
+        }
+
+        if (existingDocuments.userId !== userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const recurviseRestore = async(documentId: Id<"documents">) => {
+            const childrenDocs = await ctx.db
+                .query("documents")
+                .withIndex("by_user_parent", (q) => (
+                    q.eq("userId", userId)
+                     .eq("parentDocument", documentId)
+                ))
+                .collect();
+
+            for (const child of childrenDocs) {
+                await ctx.db.patch(child._id, {
+                    isArchived: false,
+                });
+
+                await recurviseRestore(child._id);
+            }
+        };
+
+        const options: Partial<Doc<"documents">> = {
+            isArchived: false,
+        }
+
+        if (existingDocuments.parentDocument) {
+            const parentDocs = await ctx.db.get(existingDocuments.parentDocument);
+
+            if (parentDocs?.isArchived) {
+                options.parentDocument = undefined;
+            }
+        }
+
+        const documents = await ctx.db.patch(args.id, options);
+        recurviseRestore(args.id);
+
+        return documents;
+    }
+});
+
+export const removeDocs = mutation({
+    args: {id: v.id("documents")},
+    handler: async(ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocuments = await ctx.db.get(args.id);
+
+        if (!existingDocuments) {
+            throw new Error("Not found");
+        }
+
+        if (existingDocuments.userId !== userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const documents = await ctx.db.delete(args.id);
+
+        return documents;
+    }
+});

44c8f04d1af3e2ee5d7fe3d0f9a756ce56c6aebd
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-07 20:06:46 +0800

    26: Documents

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,8 +1,12 @@
 "use client"
 
-import { Doc, Id } from "@/convex/_generated/dataModel";
+import { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import TextareaAutoSize from "react-textarea-autosize";
 
-import { SmilePlus, X } from "lucide-react";
+import { ImagePlus, SmilePlus, X } from "lucide-react";
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
 
 import { Button } from "@/components/ui/button";
 
@@ -17,6 +21,42 @@ export const Toolbar = ({
     initialData,
     preview
 }: ToolbarProps) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [isValue, setIsValue] = useState(initialData.title);
+
+    const inputRef = useRef<ElementRef<"textarea">>(null);
+
+    const update = useMutation(api.documents.update);
+
+    const enableInput = () => {
+        if (preview) return;
+
+        setIsEditing(true);
+        setTimeout(() => {
+            setIsValue(initialData.title);
+            inputRef.current?.focus();
+        }, 0);
+    };
+
+    const disableInput = () => {
+        setIsEditing(false);
+    };
+
+    const onInput = (value: string) => {
+        setIsValue(value);
+        update({
+            id: initialData._id,
+            title: value || "Untitled",
+        })
+    };
+
+    const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            disableInput(); 
+        }
+    };
+
     return (
         <div className="group relative pl-[54px]">
             {!!initialData.icon && !preview && (
@@ -41,7 +81,7 @@ export const Toolbar = ({
                     {initialData.icon}
                 </p>
             )}
-            <div className="flex items-center gap-x-1 opacity-100 group-hover:opacity-100 py-4">
+            <div className="flex items-center gap-x-1 opacity-0 group-hover:opacity-100 py-4">
                 {!initialData.icon && !preview && (
                     <DocsEmojiPicker
                         asChild
@@ -57,7 +97,35 @@ export const Toolbar = ({
                         </Button>
                     </DocsEmojiPicker>
                 )}
+                {!initialData.coverImage && !preview && (
+                    <Button 
+                        size="sm"
+                        variant="outline"
+                        className="text-muted-foreground text-xs"
+                        onClick={() => {}}
+                    >
+                        <ImagePlus className="h-4 w-4 mr-2" />
+                        Add cover
+                    </Button>
+                )}
             </div>
+            {isEditing && !preview ? (
+                <TextareaAutoSize 
+                    value={isValue}
+                    ref={inputRef}
+                    onBlur={disableInput}
+                    onKeyDown={onKeyDown}
+                    onChange={(event) => onInput(event.target.value)}
+                    className="text-5xl bg-transparent font-bold break-words outline-none text-[#3A3939] dark:text-[#CFCFCF] resize-none"
+                />
+            ): (
+                <div 
+                    className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3A3939] dark:text-[#CFCFCF]"
+                    onClick={enableInput}
+                >
+                    {initialData.title}
+                </div>
+            )}
         </div>
     )
 };

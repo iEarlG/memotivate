07837a651f42ff14e428d40e74384b21f933f745
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-15 21:33:47 +0800

    31: Advance Image Upload

--- hooks/useCoverImage.tsx
+++ hooks/useCoverImage.tsx
@@ -10,7 +10,7 @@ type CoverImageStore = {
 
 export const useCoverImage = create<CoverImageStore>((set) => ({
     isOpen: false,
-    onOpen: () => set({ isOpen: true }),
+    onOpen: () => set({ isOpen: true, url: undefined }),
     onClose: () => set({ isOpen: false, url: undefined }),
     onReplace: (url: string) => set({ isOpen: true, url }),
 }));

9c7185bb38355168446f46a3500bb310c4af49c1
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-15 22:27:16 +0800

    33: UI Aligning

--- components/main/Items.tsx
+++ components/main/Items.tsx
@@ -113,7 +113,7 @@ export const Items = ({
                     {documentsIcon}
                 </div>
             ): (
-                <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+                <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
             )}
             <span className="truncate">{label}</span>
             {isSearch && (

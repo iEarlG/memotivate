b8e71e391a3200b676118ba16f4d00234dd52800
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-15 23:49:55 +0800

    000: Fix Search & BG Color

--- components/search/SearchCommand.tsx
+++ components/search/SearchCommand.tsx
@@ -58,7 +58,7 @@ export const SearchCommand = () => {
                             title={document.title}
                             key={document._id}
                             value={`${document._id}-${document.title}`}
-                            onSelect={onSelect}
+                            onSelect={() => onSelect(document._id)}
                         >
                             {document.icon ? (
                                 <p className="mr-2 text-[18px]">
